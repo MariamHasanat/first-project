2d70c229bd7ae1029527bfde57897fe7ad63aafe
Mariam Hasanat <hasanattmariam@gmail.com>
2025-07-02 14:21:50 +0300

    feat: update CORS configuration to allow requests from Vite default port; remove commented-out resource routes for components, orders, and categories

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,8 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8000'], // Vite default port
+    'allowed_origins' => ['http://localhost:8000', 'http://localhost:3000'], // Vite default port
+
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
